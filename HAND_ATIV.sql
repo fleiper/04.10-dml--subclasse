aef5589a3e6fd222e934c79c390c72836ebae448
fleiper <162451626+fleiper@users.noreply.github.com>
2024-10-04 12:06:11 -0300

    HAND_ATIV.sql

--- HAND_ATIV.sql
+++ HAND_ATIV.sql
@@ -11,18 +11,18 @@ SETOR VARCHAR (50)
 );
 
 insert into dados (nome, sexo, bairro, salario, setor)
-values ("Fabio", "M", "SHOPPING", 4500.00, "Academico"),
-("Sandra", "F", "SÃO MIGUEL ARCANJO", 3500.00, "Academico"),
-("Vivian", "F", "CAMPINAS", 8000.00, "Academico"),
-("Adauto", "M", "SOROCABA", 4500.00, "Academico"),
-("Adriano", "M", "CENTRO", 4500.00, "Academico"),
-("Vanessa", "M", "SESI", 0.01, "Presidiario"),
-("Rodrigo Raffa", "M", "AMERICANA", 3000.00, "Academico"),
-("Rodrigão", "M", "ALGUM LUGAR", 4500.00, "Academico"),
-("Deyvini", "M", "POLICIA", 4500.00, "Academico"),
-("Reinaldo", "M", "", 100000.00, "Academico"),
-("Bruna", "F", "Instituto federal", 2500.00, "Academico"),
-("Luiz Fernando", "M", "SÃO PAULO", 4000.00, "Academico");
+values ("Larrisa Menezes", "F", "Jubaquara", 1200.00, "Marketing"),
+("Selma Nunes", "F", "grajau", 3800.00, "Vendas"),
+("Leandro Henrique", "M", "Socorro", 2950.00, "Rh"),
+("Amélia Jeremias", "F", "Socorro", 4200.00, "Marketing"),
+("Clauido Jorge", "M", "Jabaquara", 1488.00, "Vendas"),
+("Luciano hulk", "M", "Pedreira", 1000.00, "Vendas"),
+("Gabriela Santos Nunes", "F", "Jurubatuba", 4150.00),
+("Rafaela Vieira Jr", "F", "Jabaquara", 700.00, "Marketing"),
+("Suzana Crispim", "F", "Grajau", 5600.00, "Produção"),
+("Sabrina Oliveira Castro", "F", "Pedreira", 2900.00, "Marketing"),
+("jarbas silva nunes", "M", "Jurubatuba", 5300.00, "Produção"),
+("ralf borges", "M", "Jabaquara", 1600.00, "Marketing");
 
 SELECT SUM(salario) FROM DADOS; #1
 
